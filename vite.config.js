30b20394f52f837307674578b6fb0bb6b3aafba3
svanst <144538702+svanst@users.noreply.github.com>
2024-07-17 09:33:42 +0200

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig, searchForWorkspaceRoot, loadEnv } from "vite";
+import autoprefixer from "autoprefixer";
+
+export default defineConfig(({ mode }) => {
+  process.env.BROWSER = loadEnv(mode, process.cwd(), "").BROWSER || "none";
+  process.env.BROWSER_ARGS =
+    loadEnv(mode, process.cwd(), "").BROWSER_ARGS || "";
+
+  return {
+    server: {
+      open: true,
+    },
+    css: {
+      postcss: {
+        plugins: [autoprefixer({})],
+      },
+    },
+  };
+});
